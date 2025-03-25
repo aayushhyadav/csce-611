f640c79b937ad988f2a8b7025f4338d995b61003
aayushy <aayushmum@gmail.com>
2025-03-25 16:58:00 -0500

    feat: implemented FIFO scheduler

--- scheduler.C
+++ scheduler.C
@@ -21,6 +21,7 @@
 #include "console.H"
 #include "utils.H"
 #include "assert.H"
+#include "machine.H"
 
 /*--------------------------------------------------------------------------*/
 /* DATA STRUCTURES */
@@ -45,20 +46,59 @@
 /*--------------------------------------------------------------------------*/
 
 Scheduler::Scheduler() {
-  assert(false);
+  queue_size = 0;
   Console::puts("Constructed Scheduler.\n");
 }
 
 void Scheduler::yield() {
-  assert(false);
+  // disable the interrupts while manipulating the ready queue
+  if (Machine::interrupts_enabled()) {
+    Machine::disable_interrupts();
+  }
+
+  if (queue_size == 0) {
+    Console::puts("Scheduler::yield ready queue is empty!\n");
+    return;
+
+  } else {
+    // fetch the thread in FIFO order
+    Thread * thread_to_schedule = ready_queue.dequeue();
+    queue_size--;
+
+    // enable the interrupts once ready queue has been manipulated
+    if (!Machine::interrupts_enabled()) {
+      Machine::enable_interrupts();
+    }
+
+    // context switch to the selected thread
+    Thread::dispatch_to(thread_to_schedule);
+  }
 }
 
 void Scheduler::resume(Thread * _thread) {
-  assert(false);
+  // disable the interrupts while manipulating the ready queue
+  if (Machine::interrupts_enabled()) {
+    Machine::disable_interrupts();
+  }
+
+  // add the thread in the ready queue
+  ready_queue.enqueue(_thread);
+  queue_size++;
+
+  // enable the interrupts once ready queue has been manipulated
+  if (!Machine::interrupts_enabled()) {
+    Machine::enable_interrupts();
+  }
+
+  Console::puts("Successfully added to the ready queue thread with ID - ");
+  Console::puti(_thread->ThreadId());
+  Console::puts("\n");
 }
 
 void Scheduler::add(Thread * _thread) {
-  assert(false);
+  // simply invoke the resume function
+  // to add the new thread in the ready queue
+  resume(_thread);
 }
 
 void Scheduler::terminate(Thread * _thread) {
